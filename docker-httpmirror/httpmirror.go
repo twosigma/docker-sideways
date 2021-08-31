d3989e2a134f5945386cc9ce50d19e5865e0a415
Pascal Lalonde <plalonde@overnet.ca>
2021-08-31 11:41:38 -0400

    Add IP ACL

--- docker-httpmirror/httpmirror.go
+++ docker-httpmirror/httpmirror.go
@@ -1,17 +1,60 @@
 package main
 
-import(
+import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
-	"fmt"
+	"strings"
 )
 
+var (
+	http_addr  = flag.String("http_addr", ":8080", "HTTP listen address")
+	https_addr = flag.String("https_addr", ":8443", "HTTPS listen address")
+	ca         = flag.String("ca", "/etc/ssl/certs/ca-certificates.crt", "CA certificates file")
+	crt        = flag.String("crt", "cert.pem", "Server certificate")
+	key        = flag.String("key", "key.pem", "Server key")
+	allow      = flag.String("allow", "127.0.0.1/32,::1/128", "Comma-separated list of allowed subnets")
+)
+
+var allowed_subnets []*net.IPNet
+
+func allowed(res http.ResponseWriter, req *http.Request) bool {
+	host, _, err := net.SplitHostPort(req.RemoteAddr)
+	if err != nil {
+		res.WriteHeader(500)
+		fmt.Fprintf(res, "Could not parse HTTP request RemoteAddr: %s\n", req.RemoteAddr)
+		return false
+	}
+
+	client_ip := net.ParseIP(host)
+	if client_ip == nil {
+		res.WriteHeader(500)
+		fmt.Fprintf(res, "HTTP request RemoteAddr IP is invalid: %s\n", host)
+		return false
+	}
+
+	for _, c := range allowed_subnets {
+		if c.Contains(client_ip) {
+			return true
+		}
+	}
+
+	res.WriteHeader(403)
+	fmt.Fprintf(res, "IP address %s is not allowed\n", client_ip)
+	return false
+}
+
 func sendPeerCerts(res http.ResponseWriter, req *http.Request) {
+	if !allowed(res, req) {
+		return
+	}
+
 	if req.TLS == nil {
 		res.WriteHeader(404)
 		fmt.Fprint(res, "/peer_certs is only available over HTTPS\n")
@@ -31,10 +74,14 @@ func sendPeerCerts(res http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		return
 	}
-	fmt.Fprint(res, string(j) + "\n")
+	fmt.Fprint(res, string(j)+"\n")
 }
 
 func sendHeaders(res http.ResponseWriter, req *http.Request) {
+	if !allowed(res, req) {
+		return
+	}
+
 	h := make(map[string][]string)
 	h["Host"] = append(h["Host"], req.Host)
 	for k, v := range req.Header {
@@ -45,7 +92,7 @@ func sendHeaders(res http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		return
 	}
-	fmt.Fprint(res, string(j) + "\n")
+	fmt.Fprint(res, string(j)+"\n")
 }
 
 func setupTLS(ca, crt, key string) (*tls.Config, error) {
@@ -72,12 +119,17 @@ func setupTLS(ca, crt, key string) (*tls.Config, error) {
 }
 
 func main() {
-	http_addr := flag.String("http_addr", ":8080", "HTTP listen address")
-	https_addr := flag.String("https_addr", ":8443", "HTTPS listen address")
-	ca := flag.String("ca", "/etc/ssl/certs/ca-certificates.crt", "CA certificates file")
-	crt := flag.String("crt", "cert.pem", "Server certificate")
-	key := flag.String("key", "key.pem", "Server key")
 	flag.Parse()
+
+	if *allow != "" {
+		for _, cidr_str := range strings.Split(*allow, ",") {
+			_, c, err := net.ParseCIDR(cidr_str)
+			if err != nil {
+				log.Fatal("bad network spec: %s", err.Error())
+			}
+			allowed_subnets = append(allowed_subnets, c)
+		}
+	}
 
 	tlsconf, err := setupTLS(*ca, *crt, *key)
 	if err != nil {
